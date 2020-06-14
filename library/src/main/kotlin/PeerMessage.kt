9c882f40fd08d8f833a968d264e837c051580bfe
Mori Levinzon <26859371+Mori-Levinzon@users.noreply.github.com>
2020-06-14 22:27:24 +0300

    doc  PeerMessage.kt and PieceIndexStats.kt

--- library/src/main/kotlin/PeerMessage.kt
+++ library/src/main/kotlin/PeerMessage.kt
@@ -1,5 +1,8 @@
 package il.ac.technion.cs.softwaredesign
 
+/*
+data class that hold all relevant info received from messages sent between peers, epically piece request and piece sent
+ */
 data class PeerMessage (
     var index: Long,
     var begin: Long,
--- library/src/main/kotlin/PieceIndexStats.kt
+++ library/src/main/kotlin/PieceIndexStats.kt
@@ -1,13 +1,14 @@
 package il.ac.technion.cs.softwaredesign
 
 import java.time.Duration
-
+/*
+data class that hold all the relevant data about a torrent piece
+ */
 data class PieceIndexStats(
         var uploaded: Long, /* Total number of bytes downloaded (can be more than the size of the torrent) */
         var downloaded: Long, /* Total number of bytes uploaded (can be more than the size of the torrent) */
         var left: Long, /* Number of bytes left to download to complete the torrent */
         var wasted: Long, /* Number of bytes downloaded then discarded */
-//        var shareRatio: Double, /* total bytes uploaded / total bytes downloaded */
         var isValid: Boolean, /* is the piece currently in hold in the DB is valid (his SHA-1 is equal to the torrent infohash pieces' 20 byte SHA1 string*/
         var leechTime: Duration, /* Amount of time this torrent was loaded, incomplete, and the client was started */
         var seedTime: Duration /* Amount of time this torrent was loaded, complete, and the client was started */
